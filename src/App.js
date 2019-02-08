ef62a7807175b57467ae75fdea9194425178998d
damianjnc <damianjnc@gmail.com>
2019-02-08 11:03:58 +0000

    props and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'damian22', age: 999},
+      {name: 'alex', age: 44},
+      {name: 'stefani', age: 62},
+     ]
+  }
+
   render() {
+
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, it's react app. </h1>
+        <button>Switch name!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}  >My hobbies: racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
       </div>
     );
   }
